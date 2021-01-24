410de3e3ba411bcc0a67cb92bee795fb0220a64b
awth13 <awth13@gmail.com>
2021-01-24 15:02:15 +0600

    fix matching of nested fragments

--- org-emphtog.el
+++ org-emphtog.el
@@ -35,91 +35,74 @@ cursor leaves the fragment. If `org-hide-emphasis-markers' is nil, the mode does
   (when org-hide-emphasis-markers
     (cond
      (org-emphtog-mode
-      (org-emphtog--set-emph-re)
       (add-hook 'post-command-hook #'org-emphtog--post-cmd nil t))
      ;; When disabled, clean up by hiding markers around current fragment, if any
      (t
       (let ((current-frag (org-emphtog--current-frag)))
 	(when current-frag
-	  (org-emphtog--hide-markers current-frag)))
+	  (org-emphtog--hide-markers (org-emphtog--get-position current-frag))))
       (remove-hook 'post-command-hook #'org-emphtog--post-cmd t)))))
 
 (defvar-local org-emphtog--prev-frag nil
   "Previous fragment that surrounded the cursor, or nil if the cursor was not
 on a fragment. This is used to track when the cursor leaves a fragment.")
 
-(defvar org-emphtog--emph-re nil
-  "Regular expression for matching emphasised and verbatim fragments.")
-
-(defun org-emphtog--set-emph-re ()
-  "Construct the emphasis/verbatim regular expression. Similar to
-`org-set-emph-re' in org-mode but without the after-markers capture group."
-  (pcase-let*
-      ((`(,pre ,post ,border ,body ,nl) org-emphasis-regexp-components)
-       (body (if (<= nl 0) body
-	       (format "%s*?\\(?:\n%s*?\\)\\{0,%d\\}" body body nl)))
-       (template
-	(format (concat "\\([%s]\\|^\\)"
-			"\\(\\([%%s]\\)\\([^%s]\\|[^%s]%s[^%s]\\)\\3\\)")
-		pre border border body border)))
-    (setq org-emphtog--emph-re (format template "*/_+=~"))))
-
 (defun org-emphtog--post-cmd ()
   "This function is executed by `post-command-hook' in `org-emphtog-mode'.
 It handles toggling fragments depending on whether the cursor entered or exited them."
-  (let ((prev-frag org-emphtog--prev-frag)
-	(current-frag (org-emphtog--current-frag)))
+  (let* ((prev-frag org-emphtog--prev-frag)
+	 (prev-frag-start (car prev-frag))
+	 (current-frag (org-emphtog--current-frag))
+	 (current-frag-start (car current-frag)))
     ;; Do nothing if fragment did not change
-    (when (not (equal prev-frag current-frag))
+    (when (not (equal prev-frag-start current-frag-start))
       ;; Current fragment is the new previous
       (setq org-emphtog--prev-frag current-frag)
       ;; Hide markers in previous fragment, if any
       (when prev-frag
-	(org-emphtog--hide-markers prev-frag))
+	(org-emphtog--hide-markers (org-emphtog--get-position prev-frag)))
       ;; Show markers in current fragment, if any
       (when current-frag
-	(org-emphtog--show-markers current-frag)))))
+	(org-emphtog--show-markers (org-emphtog--get-position current-frag))))))
 
 (defun org-emphtog--current-frag ()
-  "Return a cons cell with locations of emphasis markers if cursor is inside
-an emphasised fragment, else return nil."
-  (when (org-in-regexp org-emphtog--emph-re 0 t)
-    (let* ((marker (match-string 3))
-	   (verbatim? (member marker '("~" "=")))
-	   (start (match-beginning 2))
-	   (end (match-end 2)))
-      ;; Code adapted from org-do-emphasis-faces
-      (when (save-excursion
-	      (goto-char start)
-	      (and
-	       ;; Do not match table hlines.
-	       (not (and (equal marker "+")
-			 (org-match-line
-			  "[ \t]*\\(|[-+]+|?\\|\\+[-+]+\\+\\)[ \t]*$")))
-	       ;; Do not match headline stars.  Do not consider
-	       ;; stars of a headline as closing marker for bold
-	       ;; markup either.
-	       (not (and (equal marker "*")
-			 (looking-at-p org-outline-regexp-bol)))
-	       ;; Do not match text without the after-markers capture group
-	       (save-excursion
-		 (backward-char)
-		 (looking-at-p (if verbatim? org-verbatim-re org-emph-re)))))
-	(cons start end)))))
+  "Return the start position and type of emphasised fragment
+if cursor is inside one, else return nil."
+  (let* ((elem (org-element-context))
+	 (elem-type (car elem))
+	 (elem-start (org-element-property :begin elem)))
+    (if (member elem-type '(bold
+			    italic
+			    underline
+			    strike-through
+			    verbatim
+			    code))
+	(cons elem-start elem-type)
+      nil)))
 
-(defun org-emphtog--show-markers (frag)
-  "Silently remove invisible property from markers."
+(defun org-emphtog--get-position (frag)
+  "Return correct position of opening and closing emphasis markers
+of an emphasised fragment FRAG."
+  (let ((start (car frag))
+	(verbatim? (member (cdr frag) '(verbatim code))))
+    (save-excursion
+      (goto-char (1- start))
+      (looking-at (if verbatim? org-verbatim-re org-emph-re))
+      (cons (match-beginning 2) (match-end 2)))))
+
+(defun org-emphtog--show-markers (match)
+  "Silently remove invisible property from markers at MATCH."
   (with-silent-modifications
-    (let ((start (car frag))
-	  (end (cdr frag)))
+    (let ((start (car match))
+	  (end (cdr match)))
       (remove-text-properties start (1+ start) '(invisible org-link))
       (remove-text-properties (1- end) end '(invisible org-link)))))
 
-(defun org-emphtog--hide-markers (frag)
-  "Silently add invisible property to markers."
+(defun org-emphtog--hide-markers (match)
+  "Silently add invisible property to markers at MATCH."
   (with-silent-modifications
-    (let ((start (car frag))
-	  (end (cdr frag)))
+    (let ((start (car match))
+	  (end (cdr match)))
       (put-text-property start (1+ start) 'invisible 'org-link)
       (put-text-property (1- end) end 'invisible 'org-link))))
 
