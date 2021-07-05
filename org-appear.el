da5149d6af4a692cf16b3df61fb00dbf2a6edf66
Alice Istleyeva <awth13@gmail.com>
2021-07-05 06:43:32 +0600

    Fix commentary typo

--- org-appear.el
+++ org-appear.el
@@ -31,7 +31,7 @@
 
 ;; This package enables automatic visibility toggling of various Org elements depending on cursor position.
 ;; It supports automatic toggling of emphasis markers, links, subscripts and
-;; superscripts, entities, and keywords. By default, toggling is instantenous
+;; superscripts, entities, and keywords. By default, toggling is instantaneous
 ;; and only affects emphasis markers. If Org mode custom variables that control
 ;; visibility of elements are configured to show hidden parts, the respective
 ;; `org-appear' settings do not have an effect.
